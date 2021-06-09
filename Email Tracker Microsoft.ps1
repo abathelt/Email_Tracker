0b3bc2319be1c96e5442fef7852d1649ecc273c3
Aleksandra <75567623+abathelt@users.noreply.github.com>
2021-06-09 21:10:44 +0200

    Add files via upload

---
+++ Email Tracker Microsoft.ps1
@@ -0,0 +1,25 @@
+﻿#Credentials are email address and AD password
+$UserCredential = Get-Credential
+$date = Get-Date
+$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid -Credential $UserCredential -Authentication Basic -AllowRedirection
+Import-PSSession $Session -DisableNameChecking
+
+Echo " "
+Echo "[START] ***************************************"
+#informations needed
+$Email = Read-Host -Prompt 'Input the user email address'
+
+Echo "[Info] Search is limited to 7 days"
+$StartDate = Read-Host -Prompt 'Input the starting date MM/DD/YYYY'
+$EndDate = Read-Host -Prompt 'Input the ending date MM/DD/YYYY'
+
+#06/06/2021
+
+Get-MessageTrace -SenderAddress $Email -StartDate $StartDate –EndDate $EndDate | Export-Csv -Path "C:\temp\MessageTrack.csv”
+Get-MessageTrace -SenderAddress $Email -Status Delivered | Export-Csv -Path "C:\temp\MessageTrack_Delivered.csv”
+
+Echo "[Info] Results were exported to C:\Temp folder"
+
+Remove-PSSession *
+
+Echo "[END] ***************************************"
